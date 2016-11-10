dbb32c5c9f1ad902db2f97e6fcb8af54e2a6095a
Christian Rodriguez <chrodriguez@gmail.com>
2016-11-10 19:01:26 -0300

    actualizo ejemplo 01 migrations

--- images/samples/15/01-migrations/db/schema.rb
+++ images/samples/15/01-migrations/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20131104190300) do
 
-  create_table "publications", force: true do |t|
+  create_table "publications", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "publication_type_id"
@@ -22,8 +21,7 @@ ActiveRecord::Schema.define(version: 20131104190300) do
     t.boolean  "single_issue"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["publication_type_id"], name: "index_publications_on_publication_type_id"
   end
-
-  add_index "publications", ["publication_type_id"], name: "index_publications_on_publication_type_id"
 
 end
