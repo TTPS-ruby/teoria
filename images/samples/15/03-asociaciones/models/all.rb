c228bd8e5bbd2db47312ab069c8a35870ac8177a
Christian Rodriguez <chrodriguez@gmail.com>
2018-11-07 10:00:13 -0300

    Mas ejemplos de asoc

--- images/samples/15/03-asociaciones/models/all.rb
+++ images/samples/15/03-asociaciones/models/all.rb
@@ -2,6 +2,8 @@ require 'bundler'
 Bundler.require
 require_relative 'customer'
 require_relative 'order'
+require_relative 'supplier'
+require_relative 'account'
 
 StandaloneMigrations::Configurator.load_configurations
 ActiveRecord::Base.establish_connection
